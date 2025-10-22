19baa1edccb7e92f1e5d33dc6c356a26daf333b8
Anton Brilliantov <brilliantov.anton@gmail.com>
2025-10-22 16:19:53 +0300

    module1-lab-Your-First-Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
     const [count, setCount] = useState(0);
@@ -15,50 +18,55 @@ function App() {
 
     return (
         <>
-            <div>
-                <div style={{'fontSize': '46px'}}>
-                    count value is {count}
-                </div>
-
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
+            <Header/>
+            <Main>
                 <div>
-                    <button onClick={() => decrement(value)}>
-                        -
-                    </button>
+                    <div style={{'fontSize': '46px'}}>
+                        count value is {count}
+                    </div>
 
-                    <input
-                        type="number"
-                        value={value} onChange={(i) => {
-                        setValue(parseInt(i.target.value < 0 ? 1 : i.target.value));
-                    }}
-                        hspace={5}
-                    />
+                    <a href="https://vite.dev" target="_blank">
+                        <img src={viteLogo} className="logo" alt="Vite logo"/>
+                    </a>
+                    <a href="https://react.dev" target="_blank">
+                        <img src={reactLogo} className="logo react" alt="React logo"/>
+                    </a>
+                </div>
+                <h1>Vite + React</h1>
+                <div className="card">
+                    <div>
+                        <button onClick={() => decrement(value)}>
+                            -
+                        </button>
 
-                    <button onClick={() => increment(value)}>
-                        +
-                    </button>
+                        <input
+                            type="number"
+                            value={value} onChange={(i) => {
+                            setValue(parseInt(i.target.value < 0 ? 1 : i.target.value));
+                        }}
+                            hspace={5}
+                        />
+
+                        <button onClick={() => increment(value)}>
+                            +
+                        </button>
+                    </div>
+                    <br/>
+                    <div>
+                        <button onClick={() => resetCount()}>
+                            reset
+                        </button>
+                    </div>
+                    <p>
+                        Edit <code>src/App.jsx</code> and save to test HMR
+                    </p>
                 </div>
-                <br/>
-                <div>
-                    <button onClick={() => resetCount()}>
-                        reset
-                    </button>
-                </div>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
+                <p className="read-the-docs">
+                    Click on the Vite and React logos to learn more
                 </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            </Main>
+            <Footer/>
+
         </>
     )
 }
